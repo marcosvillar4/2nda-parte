5a30ad3955d6acd6779a6386f91f38d523b2ffe0
Elpato99 <patozensualon@gmail.com>
2023-05-27 16:22:31 -0300

    hh

--- Main.py
+++ Main.py
@@ -8,8 +8,13 @@ vottotal = []
 absolute_path = os.path.dirname(__file__)
 relative_path = "csv"
 cadena = suf.registro()
+
 for i in range (1, cadena):
-    votwrite.csvwrite(absolute_path, vottotal)
+    error = votwrite.csvwrite()
+    if error == True:
+       i = i - 1
+    else: 
+        votwrite.csvwrite(absolute_path, vottotal)
    
 
 os.system("PAUSE")
