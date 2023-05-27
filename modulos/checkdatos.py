782cdc3566515fdf47b52cd04dbf0b9abf20dc7a
Marcos_Villar <marcos.villar.mail@gmail.com>
2023-05-27 14:19:30 -0300

    + check region + fix main

--- modulos/checkdatos.py
+++ modulos/checkdatos.py
@@ -2,27 +2,39 @@
 def vervoto(listatot, path):
     import modulos.checkdnival as dni           #Import check valores de dni
     import modulos.check_region as region
-    listdniactual = []                          # Lista temporal para candidatos votados
-    votoactual = []                             # Lista temporal para el voto actual
+    listcanactual = []                          # Lista temporal para candidatos votados
+    listregactual = []
+    votoactual = []                          # Lista temporal para el voto actual
+    
+    indexdni = 0
 
-    def find(lista, valor):             # Buscamos el dni en la lista
+    valido = False  
+    error = False  
+
+    def find(lista, valor, indexdni):             # Buscamos el dni en la lista
         try:
             lista.index(valor)
-            check(lista)
-            return 
+            checkcan(lista)
+            indexdni = indexdni + 1
+            checkreg(lista)
+            return indexdni
         except ValueError:
-            return 0
+            return indexdni
     
-    def check(lista):
-        listdniactual
-        listdniactual.append(lista[2])  # Buscamos el valor representante del candidato
+    def checkcan(lista):
+        listcanactual
+        listcanactual.append(lista[2])  # Buscamos el valor representante del candidato
+
+    def checkreg(lista):
+        listregactual
+        listregactual.append(lista[1])
 
     def indexcheck(valactual, listdni):
         if listdni.count(valactual) != 1:
             return True
         else:
             return False
-
+    
     
     valor = str(dni.checkdni())
     reg = str(region.check(path))
@@ -52,17 +64,38 @@ def vervoto(listatot, path):
 
     for nrolistactual in range(0,cantlist):
         listactual = listatot[nrolistactual]
-        find(listactual, valor)
+        indexdni = find(listactual, valor, indexdni)
 
+    
+    
+    for regindexpos in range (0, len(listregactual)):
+        if len(listregactual) == 1:
+            valido = True
+        if listregactual[regindexpos] == listregactual[0]:
+            valido = True
+        elif listregactual[regindexpos] != listregactual[0]:
+            valido = False
+
+        if valido == False:
+            print("Voto no valido, usted ya voto en otra region")
+            print(listatot)
+            error = True
+            break
+            
+
+    
     rep = False
-    for indexpos in range(0, len(listdniactual)):
-        valactual = listdniactual[indexpos]
-        rep = indexcheck(valactual, listdniactual)
+    for indexpos in range(0, len(listcanactual)):
+        valactual = listcanactual[indexpos]
+        rep = indexcheck(valactual, listcanactual)
         if rep == True:
             print("Voto no valido, DNI ya voto para esta categoria")
-            listatot.pop()
+            error = True
             break
         
+    if error == True:
+        listatot.pop()
+            
     print(listatot)
 
     return listatot
